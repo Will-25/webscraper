c29b4ce7bd4be93c706e27f04b9d011c362a6e54
Will <Will.irvin18@gmail.com>
2019-12-22 17:53:06 -0500

    ..

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cheerio = require("cheerio");
 const axios = require("axios");
 const db = require("./models")
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
